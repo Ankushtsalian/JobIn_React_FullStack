727c6bad7cde1b0360248f6a6a8a39557079f336
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-07 16:44:53 +0530

    created register form with Form row reusable component

---
+++ src/components/FormRow.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const FormRow = (props) => {
+  const { type, name, value, handleChange, labelText } = props;
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {name}
+      </label>
+
+      <input
+        type={type}
+        value={value}
+        name={name}
+        onChange={handleChange}
+        className="form-input"
+      />
+    </div>
+  );
+};
+
+export default FormRow;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,63 @@
+import { useState, useEffect } from "react";
+import { Logo, FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+  const { name, email, password, isMember } = values;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        {/* name field */}
+        <FormRow
+          type="text"
+          name="name"
+          value={name}
+          handleChange={handleChange}
+        />
+
+        {/* email field */}
+        <FormRow
+          type="email"
+          name="email"
+          value={email}
+          handleChange={handleChange}
+        />
+
+        {/* password field */}
+        <FormRow
+          type="password"
+          name="password"
+          value={password}
+          handleChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
