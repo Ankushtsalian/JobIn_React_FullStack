5a33a1c9ce618955d6cc4da81a2b002cf114458a
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-07 14:05:43 +0530

    landing page with logo and reg button style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import Landing from "./pages/Landing";
 
 function App() {
   return (
-    <main>
-      <h1>Hello Jobster</h1>
+    <>
       <Landing />
-    </main>
+    </>
   );
 }
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,29 @@ import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobIn logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>Tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="jobIn hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
