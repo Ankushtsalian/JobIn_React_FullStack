df5a9a6e85b398ccee3589f9d4a78bd37d67cf54
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-08 21:34:44 +0530

    logout reducer to remove user

--- src/pages/dashboard/SharedLayout.jsx
+++ src/pages/dashboard/SharedLayout.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { SmallSidebar, BigSidebar, NavBar } from "../../components";
 import Wrapper from "../../assets/wrappers/SharedLayout";
+import { useSelector } from "react-redux";
 
 const SharedLayout = () => {
+  const { user } = useSelector((state) => state.user);
   return (
     <Wrapper>
       <main className="dashboard">
