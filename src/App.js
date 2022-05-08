8ed969a4e168cdd8e4a22b0f936a8d744942e460
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-08 13:37:23 +0530

    CREATED NESTED ROUTES FOR DASHBOARD AS SHARED LAYOUT

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import { Routes, Route } from "react-router-dom";
-import { Landing, Error, Dashboard, Register } from "./pages";
+import { Landing, Error, Register } from "./pages";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import {
+  SharedLayout,
+  Profile,
+  AddJob,
+  AllJobs,
+  Stats,
+} from "./pages/dashboard/index";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+
         <Route path="landing" element={<Landing />} />
         <Route path="register" element={<Register />} />
         <Route path="*" element={<Error />} />
       </Routes>
+
       <ToastContainer position="top-center" />
     </>
   );
