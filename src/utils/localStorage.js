c1c67dd6c4e08296a39a813a883d8c9da472fb51
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-08 11:44:11 +0530

    created local storage for user

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -2,11 +2,11 @@ export const addUserToLocalStorage = (user) => {
   localStorage.setItem("user", JSON.stringify(user));
 };
 
-export const removeUserFromLocalStorage = (user) => {
+export const removeUserFromLocalStorage = () => {
   localStorage.removeItem("user");
 };
 
-export const getUserFromLocalStorage = (user) => {
+export const getUserFromLocalStorage = () => {
   const result = localStorage.getItem("user");
   const user = result ? JSON.parse(result) : null;
   return user;
