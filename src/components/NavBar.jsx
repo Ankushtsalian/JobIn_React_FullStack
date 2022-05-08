c0c8ba3d5db6b4ca21b7b5c1b46cd24f49e6174a
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-08 20:38:52 +0530

    logout reducer to remove user

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,19 +3,23 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Logo from "./Logo";
 import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
-import { toggleSidebar } from "../feature/user/userSlice";
+import { logoutUser, toggleSidebar } from "../feature/user/userSlice";
 
 const NavBar = () => {
   const [showLogout, setShowLogout] = useState(false);
   const { user } = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
-  const toggleButton = () => {
+  const toggleUserButton = () => {
     setShowLogout(!showLogout);
   };
 
   const toggle = () => {
     dispatch(toggleSidebar());
+  };
+
+  const logout = () => {
+    dispatch(logoutUser());
   };
 
   return (
@@ -29,17 +33,13 @@ const NavBar = () => {
           <h3 className="logo-text">dashboard</h3>
         </div>
         <div className="btn-container">
-          <button type="button" className="btn" onClick={toggleButton}>
+          <button type="button" className="btn" onClick={toggleUserButton}>
             <FaUserCircle />
             {user?.name}
             <FaCaretDown />
           </button>
           <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
-            <button
-              type="button"
-              className="dropdown-btn"
-              onClick={toggleButton}
-            >
+            <button type="button" className="dropdown-btn" onClick={logout}>
               Logout
             </button>
           </div>
--- src/feature/user/userSlice.js
+++ src/feature/user/userSlice.js
@@ -4,6 +4,7 @@ import customFetch from "../../utils/axios";
 import {
   addUserToLocalStorage,
   getUserFromLocalStorage,
+  removeUserFromLocalStorage,
 } from "../../utils/localStorage";
 
 const initialState = {
@@ -51,6 +52,11 @@ const userSlice = createSlice({
     toggleSidebar: (state) => {
       state.isSidebarOpen = !state.isSidebarOpen;
     },
+    logoutUser: (state) => {
+      state.user = null;
+      state.isSidebarOpen = false;
+      removeUserFromLocalStorage();
+    },
   },
   extraReducers: {
     [registerUser.pending]: (state) => {
@@ -91,5 +97,5 @@ const userSlice = createSlice({
   },
 });
 
-export const { toggleSidebar } = userSlice.actions;
+export const { toggleSidebar, logoutUser } = userSlice.actions;
 export default userSlice.reducer;
