ee5ab5232bf4a6125e2acc0ee06ebdaf0ae9c948
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-09 16:32:56 +0530

    created new comp NavLinks for reusability btw big and small side bars dfrt links

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,10 +1,10 @@
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import { FaTimes } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
+
 import Logo from "./Logo";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleSidebar } from "../feature/user/userSlice";
-import links from "../utils/links";
+import NavLinks from "./NavLinks";
 
 const SmallSidebar = () => {
   const { isSidebarOpen } = useSelector((state) => state.user);
@@ -27,25 +27,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">
-            {links.map((link) => {
-              const { text, path, id, icon } = link;
-
-              return (
-                <NavLink
-                  to={path}
-                  key={id}
-                  onClick={toggle}
-                  className={({ isActive }) =>
-                    isActive ? "nav-link active" : "nav-link"
-                  }
-                >
-                  <span className="icon">{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks toggleSidebar={toggle} />
         </div>
       </div>
     </Wrapper>
