ffe212a2470edf74020098a606e9e36de6470d6d
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-09 13:11:56 +0530

    created smallSidebar and using show-sidebar from assets wrapper also by media querry @992px displayed component

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,8 +1,28 @@
-import React from "react";
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import { useSelector, useDispatch } from "react-redux";
 
 const SmallSidebar = () => {
-  return <Wrapper>SmallSidebar</Wrapper>;
+  const toggle = () => {
+    console.log("toggle");
+  };
+  return (
+    <Wrapper>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button className="close-btn" onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">NavLink</div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;
