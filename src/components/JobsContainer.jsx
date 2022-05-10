6ec258e804bde44d6e2c806c24784c1e9e74e371
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-10 21:46:29 +0530

    created SearchContainer JobsContainer

---
+++ src/components/JobsContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const JobsContainer = () => {
+  return <h3>jobsContainer</h3>;
+};
+
+export default JobsContainer;
--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,16 @@ import NavBar from "./NavBar";
 import SmallSidebar from "./SmallSidebar";
 import BigSidebar from "./BigSidebar";
 import FormRowSelect from "./FormRowSelect";
+import JobsContainer from "./JobsContainer";
+import SearchContainer from "./SearchContainer";
 
-export { Logo, FormRow, NavBar, SmallSidebar, BigSidebar, FormRowSelect };
+export {
+  Logo,
+  FormRow,
+  NavBar,
+  SmallSidebar,
+  BigSidebar,
+  FormRowSelect,
+  SearchContainer,
+  JobsContainer,
+};
