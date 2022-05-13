8ecd878d6c13226cf27d2e525189b4bb4b593cc9
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-13 15:30:20 +0530

    charts updated

--- src/components/ChartsContainer.jsx
+++ src/components/ChartsContainer.jsx
@@ -3,17 +3,41 @@ import Wrapper from "../assets/wrappers/ChartsContainer";
 import { useSelector } from "react-redux";
 import AreaChartComponent from "./AreaChart";
 import BarChartComponent from "./BarChart";
+import { FaUnderline } from "react-icons/fa";
 
 export default function ChartsContainer() {
   const [barChart, setBarChart] = useState(true);
   const { monthlyApplications: data } = useSelector((state) => state.allJobs);
-
   return (
     <Wrapper>
       <h4>Monthly Applications</h4>
-      <button type="button" onClick={() => setBarChart(!barChart)}>
-        {barChart ? "Bar Chart" : "Area Chart"}
+
+      <button
+        type="button"
+        onClick={() => setBarChart(!barChart)}
+        disabled={barChart}
+        style={
+          barChart
+            ? { color: "#2cb1bc", textDecorationLine: "underline" }
+            : { color: "#000" }
+        }
+      >
+        Bar Chart
       </button>
+
+      <button
+        type="button"
+        onClick={() => setBarChart(!barChart)}
+        disabled={!barChart}
+        style={
+          !barChart
+            ? { color: "#2cb1bc", textDecorationLine: "underline" }
+            : { color: "#000" }
+        }
+      >
+        Area Chart
+      </button>
+
       {barChart ? (
         <BarChartComponent data={data} />
       ) : (
