f7457f90699f249c5834be71393d6421940a58e1
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-13 22:15:06 +0530

    PAGINATION created

--- src/components/PageBtnContainer.jsx
+++ src/components/PageBtnContainer.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
+import Wrapper from "../assets/wrappers/PageBtnContainer";
+import { useSelector, useDispatch } from "react-redux";
 
 const PageBtnContainer = () => {
-  return <div>PageBtnContainer</div>;
+  const { numOfPages, page } = useSelector((state) => state.allJobs);
+  const dispatch = useDispatch();
+
+  // const pages = Array.from({ length: numOfPages }, (_, index) => index + 1);
+  const pages = [...Array(numOfPages).keys(), numOfPages].slice(1);
+  const nextPage = () => {};
+  const prevPage = () => {};
+
+  return (
+    <Wrapper>
+      <button className="prev-btn" onClick={prevPage}>
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <div className="btn-container">
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              type="button"
+              key={pageNumber}
+              className={pageNumber === page ? "pageBtn active" : "pageBtn"}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button className="next-btn" onClick={nextPage}>
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
 };
 
 export default PageBtnContainer;
