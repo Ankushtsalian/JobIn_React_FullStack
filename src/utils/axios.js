27a67a8e57ca9bf2ca88bb2880ae160e349a0034
Ankushtsalian <ankushtsalian@gmail.com>
2022-05-15 13:09:11 +0530

    created checkForUnauthorizedResponse if user tonen expired or invalid just logout

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,15 @@
 import axios from "axios";
-
+import { clearStore } from "../feature/user/userSlice";
 const customFetch = axios.create({
   baseURL: "https://jobify-prod.herokuapp.com/api/v1/toolkit",
 });
 
 export default customFetch;
+
+export const checkForUnauthorizedResponse = (error, thunkApi) => {
+  if (error.response.status === 401) {
+    thunkApi.dispatch(clearStore());
+    return thunkApi.rejectWithValue("Unauthorized! Logging Out...");
+  }
+  return thunkApi.rejectWithValue(error.response.data.msg);
+};
